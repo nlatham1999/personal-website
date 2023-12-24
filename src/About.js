b74e9208f797b1006cf6941ddf33f640924d6231
nlatham1999 <nicholaslatham1999@gmail.com>
2023-12-24 13:55:09 -0800

    Update About.js

--- src/About.js
+++ src/About.js
@@ -8,14 +8,14 @@ const About = () => {
         <Container style={{marginLeft: "1vw", marginRight: "1vw", marginTop: "1vw"}}>
         <div className='content'>
             <h>About Me</h>
-            <p>Hi, name is Nick Latham and I am from Post Falls, Idaho. In the spring of 2021, I graduated Gonzaga 
+            <p>Hi, my name is Nick Latham and I am from Post Falls, Idaho. In the spring of 2021, I graduated Gonzaga 
                 University with a Bachelor of Science degree. I majored in Computer Science and minored in Mathematics</p>
             <p>Since January 2023 I have been with Kochava as a software developer</p>
-            <p>From June 2021 - December 2022 I was as a software developer with FAST Enterprises.</p>
+            <p>From June 2021 until December 2022 I was a software developer with FAST Enterprises.</p>
             <p>Before that, during the 2020 summer, I interned at Chief Architect Software</p>
             <p>In my free time, I love to hike and run. During the winter, I also love to ski. I am also an aspiring rock climber and mountain biker</p>
             <p>I also love to read so if you have any recomendations on what I should read next - let me know! </p>
-            <p> While I was at Gonzaga, I was a member of the Gonzaga Robotics Club and on the embedded systems team of 
+            <p>While I was at Gonzaga, I was a member of the Gonzaga Robotics Club and on the embedded systems team of 
                 Gonzaga RoboSub. I also volunteered at Blessings Under the Bridge and volunteered at the saturday math 
                 tutoring program at Gonzaga
             </p>
